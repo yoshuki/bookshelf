b9e01e2cf72c4a4b5ec93daef7d113edee7bda06
MIKAMI Yoshiyuki <yoshuki@saikyoline.jp>
2019-04-15 10:23:19 +0900

    Revert "Change basic auth condition."
    This reverts commit 13bd3e93a4e318523e0acd5c445c8f781be86736.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  http_basic_authenticate_with name: 'user', password: ENV['BASIC_AUTH_PASSWORD'] unless Rails.env.production?
+  http_basic_authenticate_with name: 'user', password: ENV['BASIC_AUTH_PASSWORD'] if Rails.env.production?
 end
