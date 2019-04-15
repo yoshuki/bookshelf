d27c8be561d8414edb4da8593b8934c2aa7a6652
MIKAMI Yoshiyuki <yoshuki@saikyoline.jp>
2019-04-15 11:31:48 +0900

    Update files under app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,2 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-
-  http_basic_authenticate_with name: 'user', password: ENV['BASIC_AUTH_PASSWORD'] if Rails.env.production?
 end
